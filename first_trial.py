ea3065b1c5b4e836ade72b610983f39bdf8df45c
Attinger Nora <nora.attinger@axa-winterthur.ch>
2016-01-04 14:38:42 +0100

    a

---
+++ first_trial.py
@@ -0,0 +1,61 @@
+
+# coding: utf-8
+
+# In[3]:
+
+get_ipython().magic(u'pylab inline')
+print "Python is easy to learn!"
+
+plot(randn(100))
+myvar = 3
+myvar +=2
+myvar
+
+
+# In[5]:
+
+get_ipython().magic(u'pylab inline')
+mydict = {"Key 1": "Value 1", 2: 3, "pi": 3.14}
+mydict
+
+
+# In[1]:
+
+get_ipython().magic(u'pylab inline')
+print("Hello World")
+
+
+# In[3]:
+
+get_ipython().magic(u'pylab inline')
+first_name = "Nora"
+last_name = "Attinger"
+print(first_name + " " + last_name)
+
+
+# In[7]:
+
+get_ipython().magic(u'pylab inline')
+shopping_list = [ 'eggs', 'milk', 'cheese', 'celery', 'peanut butter', 'baking soda' ]
+for i in shopping_list:
+    print(i)
+
+
+# In[9]:
+
+get_ipython().magic(u'pylab inline')
+import time
+print(time.localtime())
+
+
+# In[12]:
+
+get_ipython().magic(u'pylab inline')
+import turtle
+t=turtle.Pen()
+
+
+# In[ ]:
+
+
+
